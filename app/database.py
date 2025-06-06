96dfde7b968d074e487a707be0b3fe3199a898e7
Kristiana <asenovakristiana@gmail.com>
2025-06-06 18:13:45 +0300

    v

--- app/database.py
+++ app/database.py
@@ -1,5 +1,12 @@
 import sqlite3
 
+DATABASE_NAME = "books.db"
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 def initialize_database():
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -9,23 +16,26 @@ def initialize_database():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         author TEXT NOT NULL,
-        genre TEXT NOT NULL,
+        genre TEXT,
+        rating REAL,
+        published_year INTEGER
+    )    
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS reviews(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        book_id INTEGER,
+        user_name TEXT NOT NULL,
         rating REAL NOT NULL,
-        published_year INTEGER NOT NULL
+        comment TEXT,
+        FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE
     )
     """)
 
-    books_data = [
-        ("1984", "George Orwell", "Dystopian", 4.9, 1949),
-        ("To Kill a Mockingbird", "Harper Lee", "Classic", 4.8, 1960),
-        ("The Great Gatsby", "F. Scott Fitzgerald", "Novel", 4.7, 1925)
-    ]
-
-    cursor.executemany("INSERT INTO books (title, author, genre, rating, published_year) VALUES (?, ?, ?, ?, ?)", books_data)
-    
     conn.commit()
     conn.close()
-    print("Database initialized with books!")
+    print("Database initialized successfully!")
 
-if __name__ == "__main__":  # Ensures it's only executed when run directly
+if __name__ == "__main__":
     initialize_database()
