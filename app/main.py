d5c454216b6410ed300e016cbd043bbba9ed04e5
Kristiana <asenovakristiana@gmail.com>
2025-06-06 18:40:17 +0300

    дф

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+from fastapi.openapi.utils import get_openapi
 from fastapi import FastAPI
 from app.routers import books, reviews
 
 app = FastAPI()
 
-app.include_router(books.router, prefix="/books", tags=["books"])
-app.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
+app.include_router(books.router, prefix="/books", tags=["Books"])
+app.include_router(reviews.router, prefix="/reviews", tags=["Reviews"])
 
 @app.get("/")
 def home():
     return {"message": "Welcome to Book API!"}
+
+@app.get("/openapi.json", include_in_schema=False)
+def custom_openapi():
+    return get_openapi(title="Book API", version="1.0", routes=app.routes)
